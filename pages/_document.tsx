a61d08f92f32602fce9b3d4c83515608d0d7f6b2
Chirilov Narcis <chirilov.narcis@yahoo.ro>
2022-06-11 19:03:38 +0300

    styled-components-ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
 
+import { Html, Head, Main, NextScript} from "next/document";
 export default function Document() {
+    
+    
     return (
         <Html lang="en">
             <Head >
