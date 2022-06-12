104a1d56e99baec95b3842f12a08fb774f1bdf88
Chirilov Narcis <chirilov.narcis@yahoo.ro>
2022-06-12 14:02:52 +0300

    ssr styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,57 @@
 
-import { Html, Head, Main, NextScript} from "next/document";
-export default function Document() {
-    
-    
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render(): JSX.Element {
     return (
-        <Html lang="en">
-            <Head >
-            <link
-            href='/fonts/Caudex-Bold.ttf'
-            rel='prefetch'
-            as='font'
-            crossOrigin='anonymous'
-          />
-          <link
-            href='/fonts/Caudex-Regular.ttf'
-            rel='prefetch'
-            as='font'
-            crossOrigin='anonymous'
-          />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+              <Html lang="en">
+                  <Head >
+                  <link
+                  href='/fonts/Caudex-Bold.ttf'
+                  rel='prefetch'
+                  as='font'
+                  crossOrigin='anonymous'
+                />
+                <link
+                  href='/fonts/Caudex-Regular.ttf'
+                  rel='prefetch'
+                  as='font'
+                  crossOrigin='anonymous'
+                />
+                  </Head>
+                  <body>
+                      <Main />
+                      <NextScript />
+                  </body>
+              </Html>
+          );
+  }
 }
